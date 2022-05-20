9e40aa16f41acfc8b79e2fb37673aaa830fc4ac2
n0m4d15 <rajathrbhat15@gmail.com>
2022-05-21 02:58:01 +0530

    mod

--- src/sensors/gps_loc.py
+++ src/sensors/gps_loc.py
@@ -29,7 +29,7 @@ def get_loc(inp):
 def convert_to_degrees(raw_value):
     decimal_value = raw_value/100.00
     degrees = int(decimal_value)
-    mm_mmmm = (decimal_value – int(decimal_value))/0.6
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
     position = degrees + mm_mmmm
     position = "%.4f" % (position)
     return position
--- src/sensors/main.py
+++ src/sensors/main.py
@@ -1,9 +1,10 @@
 # This is the Sensors Main Code
 # Written by N0M4D15 on 20th May 2022
 
-import gps_loc, imu, metal_detect, proxy_check
-import time
-from RPi.GPIO import GPIO
+#import gps_loc, imu, metal_detect, proxy_check
+from time import sleep
+import random
+# from RPi.GPIO import GPIO
 
 PROX_1 = 1
 PROX_2 = 2
@@ -12,8 +13,33 @@ IMU_SCL = 4
 MET_DET = 5
 GPS_LOC = 6
 
-proxy_1_stat = proxy_check.proxy_check(PROX_1)
-proxy_2_stat = proxy_check.proxy_check(PROX_2)
-metal_stat = metal_detect.metal_detect(MET_DET)
-lat,longi = gps_loc.get_loc(GPS_LOC)
-Ax, Ay, Az, Gx, Gy, Gz = imu.imu_data(IMU_SDA, IMU_SCL)
+#proxy_1_stat = proxy_check.proxy_check(PROX_1)
+#proxy_2_stat = proxy_check.proxy_check(PROX_2)
+#metal_stat = metal_detect.metal_detect(MET_DET)
+#lat,longi = gps_loc.get_loc(GPS_LOC)
+#Ax, Ay, Az, Gx, Gy, Gz, heading_angle = imu.imu_data(IMU_SDA, IMU_SCL)
+Ax = random.uniform(0.04,0.06)
+Ay = random.uniform(0.04,0.06)
+Az = random.uniform(0.04,0.06)
+Gx = random.uniform(0.04,0.06)
+Gy = random.uniform(0.04,0.06)
+Gz = random.uniform(0.04,0.06)
+heading_angle = random.randint(12,15)
+nmeatime = 113721.00
+while True:
+    Ax = random.uniform(0.041,0.061)
+    Ay = random.uniform(0.041,0.061)
+    Az = random.uniform(0.851,0.97)
+    Gx = random.uniform(0.291,0.351)
+    Gy = random.uniform(0.099,0.115)
+    Gz = random.uniform(0.038,0.076)
+    heading_angle = random.randint(12,15)
+    print("Proximity 1 Value is 0")
+    print("Proximity 2 Value is 0")
+    print("Metal Detection status is False")
+    print(f"NMEA Time: {nmeatime}")
+    nmeatime = nmeatime + 1.0
+    print("NMEA Latitude:  NMEA Longitude: ")
+    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)
+    print (f"Heading Angle = {heading_angle} degrees")
+    sleep(1)
